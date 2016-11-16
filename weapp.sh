8b885318222d870895f748862dfd3fbf489b59bb
赵吉彤 <zjtong3576@sina.com>
2016-11-16 15:04:09 +0800

    all add

--- weapp.sh
+++ weapp.sh
@@ -142,36 +142,29 @@ do
   fi
 
   cd ${PagePath}
-  echo "/**
-    * ${send} By ${1}
-    * ${PagePath}.js
-    */
+echo "/**
+	* ${send} By ${1}
+	* ${PagePath}.js
+	*/
 
-  var app = getApp()
-  Page({
-    data: {
-      motto: 'Hello World',
-      userInfo: {}
-    },
-    //事件处理函数
-    bindViewTap: function() {
-      wx.navigateTo({
-        url: '../logs/logs'
-      })
-    },
-    onLoad: function () {
-      console.log('onLoad')
-      var that = this
-      //调用应用实例的方法获取全局数据
-      app.getUserInfo(function(userInfo){
-        //更新数据
-    	that.setData({
-          userInfo:userInfo
-        })
-      })
-    }
-  })" > ${PagePath}.js
-  cd ..
+Page({
+	data: {
+	},
+	onLoad: function () {
+		console.log('${PagePath} Load Success');
+	}
+})" > ${PagePath}.js
+
+echo "<!--${PagePath}.wxml-->" > ${PagePath}.wxml
+
+echo "/**
+	* ${send} By ${1}
+	* ${PagePath}.wxss
+	*/" > ${PagePath}.wxss
+
+echo "{}" > ${PagePath}.json
+
+cd ..
 
 done
 
